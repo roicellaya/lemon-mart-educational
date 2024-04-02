b578f75663a805c36c7d51ed8d3379c8c045bb16
Roicel Laya <roicel.laya@gmail.com>
2024-04-01 22:49:25 -0300

    Add lazy loaded manager module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,9 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  {
+    path: 'manager',
+    loadChildren: () => import('./manager/manager.module').then((m) => m.ManagerModule),
+  },
   { path: '**', component: PageNotFoundComponent },
 ]
