bff05a957e1fa834ad23cc58e18a289b32df772f
Roicel Laya <roicel.laya@gmail.com>
2024-04-15 17:47:17 -0400

    Add pos module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,5 +14,9 @@ export const routes: Routes = [
     path: 'user',
     loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
   },
+  {
+    path: 'pos',
+    loadChildren: () => import('./pos/pos.module').then((m) =>m.PosModule),
+  },
   { path: '**', component: PageNotFoundComponent },
 ]
---
+++ src/app/pos/pos/pos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-pos',
+  standalone: true,
+  imports: [],
+  templateUrl: './pos.component.html',
+  styleUrl: './pos.component.scss',
+})
+export class PosComponent {}
