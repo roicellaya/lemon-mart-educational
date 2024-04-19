2f687fbbdb5463f0bfcbf75aeb2849c1538ba6ea
Roicel Laya <roicel.laya@gmail.com>
2024-04-19 12:46:27 -0400

    Add in memory auth service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,17 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideRouter } from '@angular/router'
 
 import { routes } from './app.routes'
+import { InMemoryAuthService } from './auth/auth.in-memory.service'
+import { AuthService } from './auth/auth.service'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient()],
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    {
+      provide: AuthService,
+      useClass: InMemoryAuthService
+    }
+  ],
 }
