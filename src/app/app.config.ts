04cc63969e3b45eeb9f86a3e6f88fc1bee97c06e
Roicel Laya <roicel.laya@gmail.com>
2024-04-26 22:23:32 -0400

    Add auth interceptor. Add environments

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { provideHttpClient } from '@angular/common/http'
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { ApplicationConfig } from '@angular/core'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 import { provideRouter } from '@angular/router'
 
 import { routes } from './app.routes'
+import { AuthHttpInterceptor } from './auth/auth.http.interceptor'
 import { InMemoryAuthService } from './auth/auth.in-memory.service'
 import { AuthService } from './auth/auth.service'
 
@@ -11,7 +12,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([AuthHttpInterceptor])
+    ),
     {
       provide: AuthService,
       useClass: InMemoryAuthService,
